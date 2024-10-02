5c006cc954b13c0b7caf97925803bd5d3deb716a
LiwiaWagner <liwia.wagner@gmail.com>
2024-10-02 12:07:18 +0200

    changes_02.10

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,6 +15,23 @@ const Home = () => {
 
   return (
     <>
+      <div className="hero-container">
+        <h3 className="name">Hello there! I’m Liwia</h3>
+        <div className="hero-intro">
+          <span>
+            I create interactive data visualization products that provide
+            insights and help people make data driven decisions.{" "}
+          </span>
+          Currently Senior Business Intelligence Analyst doing research, design
+          and development of finance reporting solutions at EcoVadis.
+        </div>
+        <div className="projects">
+          <h2 className="projects-title">TOP PROJECTS</h2>
+        </div>
+        <div className="projects">
+          <h2 className="projects-title">ALL PROJECTS</h2>
+        </div>
+      </div>
       <div id="view" style={{ marginTop: 1000 }}>
         VIEW
       </div>
