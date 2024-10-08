6ff3d98620e0b6df7b20be2cc06efa246126e1a8
LiwiaWagner <liwia.wagner@gmail.com>
2024-10-08 12:17:23 +0200

    changes_08.10_1

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,12 +3,16 @@ import React from "react";
 function Card(props) {
   const card = props.card;
   return (
-    <div>
-      <p>{card.title}</p>
-      <p>{card.description}</p>
-      <p>{card.tools}</p>
-      <div className="img-desktop">{card.img_desktop}</div>
-      <div className="img-mobile">{card.img_mobile}</div>
+    <div className="card-container">
+      <div>
+        <p>{card.title}</p>
+        <p>{card.description}</p>
+        <p>{card.tools}</p>
+      </div>
+      <div>
+        <div className="img-desktop">{card.img_desktop}</div>
+        <div className="img-mobile">{card.img_mobile}</div>
+      </div>
     </div>
   );
 }
