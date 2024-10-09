91fe648b940696bcb0f2c238e08cb6d32cff1da2
LiwiaWagner <liwia.wagner@gmail.com>
2024-10-09 13:53:39 +0200

    changes_09.10_1

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,17 +3,34 @@ import React from "react";
 function Card(props) {
   const card = props.card;
   return (
-    <div className="card-container">
-      <div className="card-container-left">
-        <p className="card-title">{card.title}</p>
-        <p className="card-description">{card.description}</p>
-        <p className="card-tools">{card.tools}</p>
-        <button className="btn-project">{card.btn_text}</button>
-      </div>
-      <div className="card-container-right">
-        <div className="img-desktop">{card.img_desktop}</div>
-        <div className="img-mobile">{card.img_mobile}</div>
-      </div>
+    <div id="four" className="project-container top-project-container-3">
+      <article>
+        <div className="card-container">
+          <div className="card-container-left">
+            <p className="card-title">{card.title}</p>
+            <p className="card-description">{card.description}</p>
+            <p className="card-tools">{card.tools}</p>
+            <button
+              className="btn-project"
+              style={{ backgroudColor: card.btn.color }}
+            >
+              {card.btn.text}
+            </button>
+          </div>
+          <div className="card-container-right">
+            <img
+              className="img-desktop"
+              src={card.images.desktop.src}
+              alt={card.images.desktop.alt}
+            />
+            <img
+              className="img-mobile"
+              src={card.images.mobile.src}
+              alt={card.images.mobile.alt}
+            />
+          </div>
+        </div>
+      </article>
     </div>
   );
 }
