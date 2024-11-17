1433df7ba114703ae990afba20de160a62068aa6
LiwiaWagner <liwia.wagner@gmail.com>
2024-11-17 18:03:15 +0100

    card-mobile-project-link

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,6 +7,8 @@ function Card(props) {
   const hasHtmlId = props.hasHtmlId;
   const hasALink = Boolean(card.btn.aLink);
   const imgPosition = props.imgPosition || "right";
+  const cardLink = hasALink ? card.btn.aLink : card.btn.refLink;
+  const linkTarget = hasALink ? "_self" : "_blank";
 
   return (
     <div
@@ -15,48 +17,55 @@ function Card(props) {
       style={{ backgroundColor: card.backgroundColor }}
     >
       <article>
-        <div
-          className={`card-container ${
-            imgPosition === "right" ? "" : "card-container-img-left"
-          }`}
+        <Link
+          to={cardLink}
+          target={linkTarget}
+          className="card-link"
+          style={{ textDecoration: "none" }}
         >
-          <div className="card-container-text">
-            <p className="card-title">{card.title}</p>
-            <p className="card-description">{card.description}</p>
-            <p className="card-tools">{card.tools}</p>
-            <Link
-              to={hasALink ? card.btn.aLink : card.btn.refLink}
-              target={hasALink ? "_self" : "_blank"}
-            >
-              <button
-                className="btn-project"
-                style={{ backgroundColor: card.btn.color }}
-              >
-                {card.btn.text}
-                {!hasALink && (
-                  <div className="btn-icon">
-                    <HiExternalLink />
-                  </div>
-                )}
-              </button>
-            </Link>
+          <div
+            className={`card-container ${
+              imgPosition === "right" ? "" : "card-container-img-left"
+            }`}
+          >
+            <div className="card-container-text">
+              <p className="card-title">{card.title}</p>
+              <p className="card-description">{card.description}</p>
+              <p className="card-tools">{card.tools}</p>
+
+              <div className="btn-container">
+                <button
+                  className="btn-project"
+                  style={{ backgroundColor: card.btn.color }}
+                >
+                  {card.btn.text}
+                  {!hasALink && (
+                    <div className="btn-icon">
+                      <HiExternalLink />
+                    </div>
+                  )}
+                </button>
+              </div>
+            </div>
+
+            <div
+              className="card-container-img-desktop card-container-img-tablet"
+              style={{
+                backgroundImage: `url(${card.images.desktop.src})`,
+                backgroundPosition: `center center`,
+              }}
+            />
+
+            <div
+              className="card-container-img-mobile"
+              style={{
+                backgroundColor: card.btn.color,
+                backgroundImage: `url(${card.images.mobile.src})`,
+                backgroundPosition: `bottom ${card.images.mobile.positionBottom} right ${card.images.mobile.positionRight}`,
+              }}
+            />
           </div>
-          <div
-            className="card-container-img-desktop card-container-img-tablet"
-            style={{
-              backgroundImage: `url(${card.images.desktop.src})`,
-              backgroundPosition: `center center`,
-            }}
-          />
-          <div
-            className="card-container-img-mobile"
-            style={{
-              backgroundColor: card.btn.color,
-              backgroundImage: `url(${card.images.mobile.src})`,
-              backgroundPosition: `bottom ${card.images.mobile.positionBottom} right ${card.images.mobile.positionRight}`,
-            }}
-          />
-        </div>
+        </Link>
       </article>
     </div>
   );
